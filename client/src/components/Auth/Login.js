7640584b50323fcf9f8271cc73a098092fec4ca9
Addison Hill <adddison.hill@gmail.com>
2021-06-23 16:56:48 -0500

    authenticate google users with Apollo-server

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 import { google } from "apollo-reporting-protobuf";
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+{
+  me {
+    _id
+    name
+    email
+    picture
+  }
+}
+`;
+
 const Login = ({ classes }) => {
-  const onSuccess = (googleUser) => {
+  const onSuccess = async (googleUser) => {
     const idToken = googleUser.getAuthResponse().id_token;
+    const client = new GraphQLClient("http://localhost:4000/graphql", {
+      headers: { authorization: idToken },
+    });
+    const data = await client.request(ME_QUERY);
   };
 
   return (
